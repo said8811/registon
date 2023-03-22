6e5e45d6cabaa819013f7cb7db63008a2b6ee4d9
Saidazam <saidolmovsaid@gmail.com>
2023-03-21 23:51:43 -0700

    hoem_page

--- lib/screens/tab_box/home/home_page.dart
+++ lib/screens/tab_box/home/home_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:registon/bloc/teachers_cubit/teachers_cubit.dart';
 import 'package:registon/bloc/teachers_cubit/teachers_state.dart';
+import 'package:registon/data/models/teachers_model/teachers_model.dart';
 import 'package:registon/data/repository/storage_repository.dart';
 
 class HomePage extends StatefulWidget {
@@ -138,6 +139,14 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             SizedBox(height: 20.h),
+            Padding(
+              padding: const EdgeInsets.only(left: 20).r,
+              child: Text(
+                "O'qituvchilar",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.sp),
+              ),
+            ),
+            SizedBox(height: 12.h),
             BlocBuilder<TeachersCubit, TeachersState>(
               builder: (context, state) {
                 if (state is TeachersInSucces) {
@@ -153,32 +162,59 @@ class _HomePageState extends State<HomePage> {
                       scrollDirection: Axis.horizontal,
                       shrinkWrap: true,
                       itemBuilder: (context, index) {
+                        TeachersModel teacher = state.taechers[index];
+
                         return Container(
                           width: 150.w,
-                          padding: EdgeInsets.symmetric(horizontal: 14.r),
                           decoration: BoxDecoration(
-                              color: Colors.orange,
+                              border: Border.all(color: Colors.orange),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "http://3.70.231.199/${state.taechers[index].imagePath}"),
+                                  fit: BoxFit.cover),
                               borderRadius: BorderRadius.circular(15)),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(height: 80.h),
-                              Text(
-                                state.taechers[index].firstName,
-                                style: TextStyle(
-                                    fontSize: 18.sp,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
+                          child: Stack(children: [
+                            Positioned(
+                              bottom: 0,
+                              child: Container(
+                                padding: EdgeInsets.symmetric(horizontal: 10.r),
+                                decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                        begin: Alignment.bottomCenter,
+                                        end: Alignment.topCenter,
+                                        colors: [
+                                          Colors.orange,
+                                          Colors.orange.withOpacity(0.24)
+                                        ]),
+                                    color: Colors.orange,
+                                    borderRadius: BorderRadius.circular(14)),
+                                height: 80.h,
+                                width: 150.w,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      height: 25.h,
+                                    ),
+                                    Text(
+                                      state.taechers[index].firstName,
+                                      style: TextStyle(
+                                          fontSize: 18.sp,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(height: 6.h),
+                                    Text(
+                                      teacher.subject,
+                                      style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w600),
+                                    )
+                                  ],
+                                ),
                               ),
-                              SizedBox(height: 6.h),
-                              const Text(
-                                "10ta o'qituvchi",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w600),
-                              )
-                            ],
-                          ),
+                            ),
+                          ]),
                         );
                       },
                     ),
