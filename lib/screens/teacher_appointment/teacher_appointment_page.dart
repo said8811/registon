c8054193f7999ac6ab98ec98af4f59f46b512e8b
Saidazam <saidolmovsaid@gmail.com>
2023-04-02 03:06:39 -0700

    changes

--- lib/screens/teacher_appointment/teacher_appointment_page.dart
+++ lib/screens/teacher_appointment/teacher_appointment_page.dart
@@ -1,8 +1,17 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:registon/screens/teacher_appointment/widgets/calendar_container.dart';
 
-class TeacherAppointmentPage extends StatelessWidget {
+class TeacherAppointmentPage extends StatefulWidget {
   const TeacherAppointmentPage({super.key});
+
+  @override
+  State<TeacherAppointmentPage> createState() => _TeacherAppointmentPageState();
+}
+
+class _TeacherAppointmentPageState extends State<TeacherAppointmentPage> {
+  int selectedDay = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +21,7 @@ class TeacherAppointmentPage extends StatelessWidget {
         backgroundColor: Colors.white,
         title: Text(
           "O'qituvchi bandlash",
-          style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
         ),
       ),
       body: Container(
@@ -20,7 +29,36 @@ class TeacherAppointmentPage extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(
-              height: 40.sp,
+              height: 20.sp,
+            ),
+            SizedBox(
+              height: 88.h,
+              child: ListView.builder(
+                itemCount: 7,
+                padding: const EdgeInsets.only(left: 24, right: 12).r,
+                scrollDirection: Axis.horizontal,
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedDay = index;
+                      });
+                    },
+                    child: CalendarContainer(
+                      select: selectedDay == index ? true : false,
+                      week: DateFormat.E().format(
+                          DateTime.fromMillisecondsSinceEpoch(
+                              DateTime.now().millisecondsSinceEpoch +
+                                  (index) * 86400000)),
+                      date: DateTime.fromMillisecondsSinceEpoch(
+                              DateTime.now().millisecondsSinceEpoch +
+                                  (index) * 86400000)
+                          .day,
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
